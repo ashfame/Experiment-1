4ef64e940c24f219c17f1e83839c54e68e1786a1
stirredo <stirredo@gmail.com>
2015-04-18 01:15:29 +0530

    -- Made slide section -- Made text animate
    TODO:
    Add background

---
+++ gulp.js
@@ -0,0 +1,33 @@
+var gulp=require('gulp');
+var minifyCSS=require('gulp-minify-css');
+var sass=require('gulp-sass');
+var concat=require('gulp-concat');
+var uglify=require('gulp-uglify');
+
+gulp.task('styles',function(){
+    return gulp.src(['./bower_components/foundation/scss/**/*.scss','./bower_components/animate.css/animate.css'])
+        .pipe(sass())
+        .pipe(minifyCSS({keepBreaks:true}))
+        .pipe(concat('content.css'))
+        .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('jsLibrary',function(){
+    return gulp.src(['./bower_components/jquery/dist/jquery.js','./bower/foundation/foundation.js'])
+        .pipe(uglify())
+        .pipe(concat('libraries.js'))
+        .pipe(gulp.dest('./public/js/'));
+});
+gulp.task('js',function(){
+    return gulp.src('./bower_components/custom/**/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./laravel/public/js/'));
+});
+
+gulp.task('watch',function(){
+    gulp.watch('./bower_components/foundation/**/*.scss',['styles']);
+
+});
+
+gulp.task('default',['styles','jsLibrary','js','watch']);
+
